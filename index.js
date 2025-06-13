1441b29b9d5f4e3bbb95c6cc62255e26bd2f8a89
Quanpham1167 <hoanganh95.hgi1167@gmail.com>
2025-06-13 11:53:44 +0700

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const mineflayer = require('mineflayer');
+// index.js const mineflayer = require('mineflayer'); const { Vec3 } = require('vec3'); const keepAlive = require('./keep_alive');
 
-const bot = mineflayer.createBot({
-  host: 'gold06.asaka.asia',
-  port: 25006,
-  username: 'TenBotAFK',
-});
+const bot = mineflayer.createBot({ host: 'gold06.asaka.asia', port: 25006, username: 'AFK' });
 
-bot.on('spawn', () => {
-  console.log('Bot đã vào server!');
-});
+bot.once('spawn', () => { console.log('Bot đã vào server.'); });
+
+bot.on('messagestr', async (message) => { const lower = message.toLowerCase(); if (lower.includes('/register') || lower.includes('đăng ký')) { bot.chat('/register 123456789@ 123456789@'); setTimeout(() => bot.chat('/login 123456789@'), 3000); setTimeout(() => bot.chat('/lay'), 5000); } else if (lower.includes('/login') || lower.includes('đăng nhập')) { bot.chat('/login 123456789@'); setTimeout(() => bot.chat('/lay'), 5000); } else if (message.includes('ʏêᴜ ᴄầᴜ ᴅịᴄʜ ᴄʜᴜʏểɴ đếɴ') || message.includes('ʜọ.')) { bot.chat('/tpaccept'); setTimeout(() => bot.chat('/lay'), 5000); } });
+
+setInterval(() => { const pos = bot.entity.position; bot.setControlState('sneak', false); bot.setControlState('forward', true); setTimeout(() => { bot.setControlState('forward', false); bot.setControlState('back', true); setTimeout(() => { bot.setControlState('back', false); bot.setControlState('jump', false); bot.setControlState('sneak', false); }, 1000); }, 1000); }, 60000);
+
